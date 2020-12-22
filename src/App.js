ae578064e5d8a3044dd304c95c8fae46b2a39e34
Evyn Newton <newton.evyn@gmail.com>
2020-12-21 22:05:19 -0500

    some basic structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Home from "./screens/Home/Home";
+import About from "./screens/About/About";
+import Dev from "./screens/Dev/Dev";
+import Theater from "./screens/Theater/Theater";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="about">
+          <About />
+        </Route>
+        <Route>
+          <Dev />
+        </Route>
+        <Route>
+          <Theater />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/shared/Nav.jsx
@@ -0,0 +1,7 @@
+export default function Nav() {
+  return (
+    <div>
+      <h3>Nav</h3>
+    </div>
+  );
+}
