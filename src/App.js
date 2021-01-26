f159eeba4b11fdc214ba0d6ea005a1e5b95f709a
Evyn Newton <newton.evyn@gmail.com>
2021-01-26 17:53:20 -0500

    home route exact path

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/">
+        <Route exact path="/">
           <Home />
         </Route>
         <Route path="/about">
